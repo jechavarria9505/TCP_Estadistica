2b2517ac8c97b0f93a5cd6f66eaae1df49471677
Jhonatan Echavarria Henao <jhonatan.echavarria@udea.edu.co>
2025-03-30 09:02:32 -0500

    Agregando evento a agregarDato

--- src/FrmEstadistica.java
+++ src/FrmEstadistica.java
@@ -2,6 +2,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -13,7 +14,12 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 
+
+
 public class FrmEstadistica  extends JFrame{
+    private JTextField txtDato;
+    private JLabel lblMuestra;
+    private JList lstMuestra;
     //Metodo constructor
     public FrmEstadistica(){
         setSize(500,300);
@@ -27,7 +33,7 @@ public class FrmEstadistica  extends JFrame{
         getContentPane().add(lblDato);        //Para añadirlo a la ventana usar y se debe usar debajo de la declaracion
 
         //Creando Caja de texto
-        JTextField txtDato=new JTextField();
+        txtDato=new JTextField();
         txtDato.setBounds(110,10,100,25);
         getContentPane().add(txtDato);
         
@@ -41,12 +47,12 @@ public class FrmEstadistica  extends JFrame{
         getContentPane().add(btnQuitar);
 
         //Creando Cuadro de lista
-        JLabel lblMuestra=new JLabel("Muestra: ");
+        lblMuestra=new JLabel("Muestra: ");
         lblMuestra.setBounds(220, 10 ,100 ,25);    
         lblMuestra.setHorizontalAlignment(SwingConstants.CENTER);    //Para Centrar
         getContentPane().add(lblMuestra);  
 
-        JList lstMuestra=new JList();
+        lstMuestra=new JList();
         JScrollPane spMuestra=new JScrollPane(lstMuestra);
         spMuestra.setBounds(220,40,100,150);
         getContentPane().add(spMuestra);
@@ -75,8 +81,7 @@ public class FrmEstadistica  extends JFrame{
 
             @Override
             public void actionPerformed(ActionEvent e) {
-               JOptionPane.showMessageDialog(null, "Hizo Click en Agregar"); //Para mostrar mensajes superpuestos
-
+                agregarDato();
             } 
 
         });
@@ -85,8 +90,7 @@ public class FrmEstadistica  extends JFrame{
 
             @Override
             public void actionPerformed(ActionEvent e) {
-               JOptionPane.showMessageDialog(null, "Hizo Click en Quitar"); //Para mostrar mensajes superpuestos
-               
+                quitarDato();
             } 
 
         });
@@ -100,6 +104,32 @@ public class FrmEstadistica  extends JFrame{
             } 
 
         });
+    }
+
+    private int MAXIMO_DATOS = 1000;
+    private double[] muestra=new double[1000];
+    private int totalDatos = 0;
+
+    private void agregarDato() { 
+            if(totalDatos<=MAXIMO_DATOS){
+                muestra[totalDatos]=Double.parseDouble(txtDato.getText());
+                totalDatos++;
+                mostrar();
+            }
+    }
+
+    private void mostrar() {
+        String[] strMuestra=new String[totalDatos];
+        for(int i=0;i<totalDatos;i++){
+            strMuestra[i]=String.valueOf(muestra[i]);
+        }
+        DefaultListModel dlm = new DefaultListModel();
+        dlm.copyInto(strMuestra);
+        lstMuestra.setListData(strMuestra);        
+    }
+
+    private void quitarDato() {
+
     }
 
 }
